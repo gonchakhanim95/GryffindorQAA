f54a19efda518dd2f4518f3d4c561eda2a15dce6
gonchakhanim95 <goncha.kanim@gmail.com>
2022-12-11 01:12:26 +0400

    HomeWorkAsStudent

---
+++ GryffindorQAA/Features/ViewHomeworkAsStudent.feature
@@ -0,0 +1,13 @@
+﻿Feature: ViewHomeworkAsStudent
+
+A short summary of the feature
+
+Scenario: View list of homework as student
+	Given Open Auth as student 
+	And fill form
+	| email         | password     |
+	| "said@mail.ru"| "salamsalam" |
+	And press button sing in
+	When Select homeworks
+	When select date
+	Then View homeworks
---
+++ GryffindorQAA/Support/Urls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GryffindorQAA.Support
+{
+    public class Urls
+    {
+        public const string HomePage = @"https://piter-education.ru:7074/login";
+        public const string HomeWorkPage = @"https://piter-education.ru:7074/homeworks";
+    }
+}
