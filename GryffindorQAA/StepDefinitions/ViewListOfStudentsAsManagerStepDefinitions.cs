51971988b9a7c7d0dffc700d02d41ee85864eb39
gonchakhanim95 <goncha.kanim@gmail.com>
2022-12-16 03:16:19 +0400

    /

--- GryffindorQAA/StepDefinitions/ViewListOfStudentsAsManagerStepDefinitions.cs
+++ GryffindorQAA/StepDefinitions/ViewListOfStudentsAsManagerStepDefinitions.cs
@@ -1,8 +1,10 @@
 using GryffindorQAA.Support;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using System;
-using TechTalk.SpecFlow;
+using GryffindorQAA.BackModel;
+using GryffindorQAA.Client;
+using Gherkin;
+using Newtonsoft.Json.Linq;
 
 namespace GryffindorQAA.StepDefinitions
 {
@@ -10,22 +12,45 @@ namespace GryffindorQAA.StepDefinitions
     public class ViewListOfStudentsAsManagerStepDefinitions
     {
         private WebDriver _driver;
+
         [Given(@"Registration as student")]
         public void GivenRegistrationAsStudent(Table table)
         {
-            throw new PendingStepException();
+            RegistrationRequestModel registrationRequestModel = new RegistrationRequestModel()
+            {
+                FirstName="Daniel",
+                LastName="Martin",
+                Patronymic="Pitt",
+                Email="drep@gmail.com",
+                Username="Drep",
+                Password="daniello",
+                City="Kurdemir",
+                BirthDate= "23.03.2003",
+                GitHubAccount= "23.03.2003",
+                PhoneNumber= "+78884445454"
+            };
+            Clientt client = new Clientt();
+            client.RegistrationStudent(registrationRequestModel);
         }
 
         [Given(@"Auth as Admin")]
         public void GivenAuthAsAdmin()
         {
-            throw new PendingStepException();
+
+            AuthRequestModel auth= new AuthRequestModel()
+            {
+                Email = "marina@example.com",
+                Password = "marina123456"
+            };
+            Clientt client = new Clientt();
+            string token = client.Auth(auth);
         }
 
         [Given(@"Get all list students")]
         public void GivenGetAllListStudents()
         {
-            throw new PendingStepException();
+            Clientt clientt = new Clientt();
+            List<UserResponseModel> user = clientt.ShowAllUsers(token);
         }
 
         [Given(@"Give the student the role of a manager")]
