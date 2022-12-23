826f25c087c420f088de65a0fdb8f90e9a7f5007
gonchakhanim95 <gonchakhanim@gmail.com>
2022-12-23 19:14:04 +0400

    new scenario

---
+++ GryffindorQAA/Features/AddProfilePhoto.feature
@@ -0,0 +1,23 @@
+﻿Feature: AddProfilePhoto
+
+A short summary of the feature
+
+@tag1
+Scenario: Add profile photo as student
+	Given Open  registration page
+	And Fill out form
+	| LastName | FirstName | Patronymic | BirthDate  | Password   | RepeatPassword | Email          | Phone        |
+	| Potter   | Harry     | James      | 31.06.1980 | helloworld | helloworld     | harry@mail.com | +78882211233 |
+	And Click  button registered
+	And Must  come out inscription "Добро пожаловать!!"
+	And Open  Auth as student 
+	And Fill out form for Auth
+	| Email          | Password   |
+	| harry@mail.com | helloworld |
+	And Press button sing in
+	When Open setting 
+	And Click on profile photo
+	And Select photo
+	And Change profile photo
+	And Click Save
+	Then Photo profile should change
---
+++ GryffindorQAA/StepDefinitions/AddProfilePhotoStepDefinitions.cs
@@ -0,0 +1,39 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace GryffindorQAA.StepDefinitions
+{
+    [Binding]
+    public class AddProfilePhotoStepDefinitions
+    {
+        [When(@"Open setting")]
+        public void WhenOpenSetting()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Click on profile photo")]
+        public void WhenClickOnProfilePhoto()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Select photo")]
+        public void WhenSelectPhoto()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Change profile photo")]
+        public void WhenChangeProfilePhoto()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"Photo profile should change")]
+        public void ThenPhotoProfileShouldChange()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
