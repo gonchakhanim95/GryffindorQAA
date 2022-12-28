c2ac11eeae416511434afd0072e63c4c37248124
gonchakhanim95 <gonchakhanim@gmail.com>
2022-12-28 11:38:55 +0400

    vse

--- GryffindorQAA/Pages/SettingPage.cs
+++ GryffindorQAA/Pages/SettingPage.cs
@@ -17,7 +17,36 @@
         public IWebElement ButtonBack => _driver.FindElement(By.XPath(@"//div[text()='Назад']")); 
         public IWebElement ButtonSetting => _driver.FindElement(By.XPath(@"//div[@class='svg-fond']"));
         public IWebElement ButtonPhotoProfile => _driver.FindElement(By.XPath(@"//span[@class='avatar-text']"));
-        public IWebElement ButtonSelectPhoto
+        public IWebElement ButtonSelectPhoto => _driver.FindElement(By.XPath(@"//input[@type='submit']"));
+        public override void Open()
+        {
+        }
+        public void ClickButtonSetting()
+        {
+            WebDriverWait driverWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
+            driverWait.Until(ExpectedConditions.ElementExists(By.XPath(@"//div[@class='svg-fond']")));
+            ButtonSetting.Click();
+        }
+        public void ClickButtonPhotoProfile()
+        {
+            WebDriverWait driverWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
+            driverWait.Until(ExpectedConditions.ElementExists(By.XPath(@"//div[@class='svg-text']")));
+            ButtonPhotoProfile.Click();
+        }
+        public void ClickButtonSelectPhoto()
+        {
+            string scrypt = "document.querySelector('#root > div.modal-background > div > div.buttons-container > label > input').setAttribute('class','display')";
+            _driver.ExecuteScript(scrypt);
+            string filePath = "C:\\Users\\goncha\\Downloads\\1102284.jpg";
+            _driver.FindElement(By.XPath("//input[@type='file']")).SendKeys(filePath);
+            ButtonSelectPhoto.Click();
+        }
+
+        public bool CheckProfilePhotoExists()
+        {
+            return _driver.FindElements(By.XPath(@"//img[@class='avatar-photo']")).Any();
+        }
+       /* public IWebElement ButtonSelectPhoto
         {
             get
             {
@@ -25,11 +54,7 @@
                 WebDriverWait driverWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
                 return driverWait.Until(ExpectedConditions.ElementExists(By.XPath(@"//img[@class='avatar-photo']")));
             }
-        }
-        public override void Open()
-        {
-            _driver.Navigate().GoToUrl(Urls.SettingPage);
-        }
+        }*/
         public void EnterLastName(string text)
         {
             DriverStorage storage = DriverStorage.GetInstance();
@@ -104,30 +129,6 @@
         public void ClickButtonBack()
         {
             ButtonBack.Click();
-        }
-        public void ClickButtonSetting()
-        {
-            WebDriverWait driverWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
-            driverWait.Until(ExpectedConditions.ElementExists(By.XPath(@"//div[@class='svg-fond']")));
-            ButtonSetting.Click();
-        }
-        public void ClickButtonPhotoProfile()
-        {
-            WebDriverWait driverWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
-            driverWait.Until(ExpectedConditions.ElementExists(By.XPath(@"//div[@class='svg-text']")));
-            ButtonPhotoProfile.Click();
-        }
-        public void ClickButtonSelectPhoto()
-        {
-            string scrypt = "document.querySelector('#root > div.modal-background > div > div.buttons-container > label > input').setAttribute('class','display')";
-            _driver.ExecuteScript(scrypt);
-            string filePath = "C:\\Users\\goncha\\Downloads\\1102284.jpg";
-            _driver.FindElement(By.XPath("//input[@type='file']")).SendKeys(filePath);
-            ButtonSelectPhoto.Click();
-        }
-        public bool CheckProfilePhotoExists()
-        {
-            return _driver.FindElements(By.XPath(@"//img[@class='avatar-photo']")).Any();
         }
     } 
 }
